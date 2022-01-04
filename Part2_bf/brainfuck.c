31b1cc2ad3ad996d8fd11b73b7a6a5425e525fae
Aren Elchinyn <slizen.can@gmail.com>
2022-01-04 16:19:55 +0300

    Add new functions

--- Part2_bf/brainfuck.c
+++ Part2_bf/brainfuck.c
@@ -3,48 +3,76 @@
 #include <stdlib.h>
 
 
-unsigned char memory[30000] = {0};
+char memory[30000] = {0};
 int cursor = 0;
 
 
-
-int execute(char *input) {
+int execute(char *input){
     char current_char;
     size_t i, loop;
 
-    for (i = 0; input[i] != 0; i++) {
-        
+    for (i = 0; input[i] != 0; i++){
         current_char = input[i];
 
-        if (current_char == '>') {
-            ++cursor;
-        } else if (current_char == '<') {
-            --cursor;
-        } else if (current_char == '+') {
-            memory[cursor]++;
-        } else if (current_char == '-') {
-            memory[cursor]--;
-        } else if (current_char == '.' ) {
-            putchar(memory[cursor]);
-        } else if (current_char == ',') {
-            memory[cursor] = getchar();
-        } else if (current_char == '[') {
-            continue;
-        } else if (current_char == ']' && memory[cursor]) {
-            loop = 1;
-            while (loop > 0) {
-                current_char = input[--i];
-                if (current_char == '[') {
-                    loop--;
-                } else if (current_char == ']') {
-                    loop++;
+        switch(current_char){
+            case '+':
+                memory[cursor]++;
+                break;
+            case '-':
+                memory[cursor]--;
+                break;
+            case '>':
+                cursor++;
+                break;
+            case '<':
+                cursor--;
+                break;
+            case '.':
+                putchar(memory[cursor]);
+                break;
+            case ',':
+                memory[cursor] = getchar();
+                break;
+            case '[':
+                break;
+            case ']':
+                if (memory[cursor]) {
+                    loop = 1;
+
+                    while ( loop > 0){
+                        current_char = input[--i];
+                        if (current_char == '['){
+                            loop--;
+                        } else if (current_char == ']'){
+                            loop++;
+                        }
+                    }
                 }
-            }
+                
+                break;
+            
+            // Non-brainfuck functions
+            case '0':
+                memory[cursor] = 0;
+                break;
+            case 'E':
+                puts("Exit with code 2");
+                return 2;
+                break;
+
+
+            default:
+                break;
+            
         }
     }
+
+    puts("Exit with code 0");
+    return 0;
 }
 
 
-int main() {
-    return execute("+.[+.]");
+int main(){
+
+    return execute("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.");
 }
